0f5cc80f45008a8ef0d68645a44f50fe51499a03
Syrone Wong <wong.syrone@gmail.com>
2020-03-04 13:28:55 +0800

    util: refine logcat output
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- util/util.go
+++ util/util.go
@@ -25,37 +25,35 @@ func LogGoRoutineCount() {
 // LogGoroutineStackTrace log goroutine stack trace to logcat on android
 func LogGoroutineStackTrace() {
 	var err error
-	var start, end int
 	bufferedWriter := bufio.NewWriterSize(log.Writer(), LogLineBufLen)
 	buf := make([]byte, MaxLogBufLen)
 	buf = buf[:runtime.Stack(buf, true)]
 	bufferedWriter.WriteString("=== BEGIN goroutine stack dump ===\n")
 	total := len(buf)
-	start = 0
-	if total < LogLineBufLen {
-		end = total - 1
-	} else {
-		end = start + LogLineBufLen
-	}
+	start := 0
+	end := 0
 	remaining := total
+	if remaining >= LogLineBufLen {
+		end += LogLineBufLen
+	} else {
+		end += remaining
+	}
+
 	log.Printf("buf len is %d\n", total)
 	for {
 		nn, err := bufferedWriter.Write(buf[start:end])
 		if err != nil {
 			panic(err)
 		}
-		if nn <= 0 {
-			break
-		}
 		start += nn
-		if end+LogLineBufLen > total-1 {
-			end = total - 1
-		} else {
-			end += LogLineBufLen
-		}
 		remaining -= nn
 		if remaining <= 0 {
 			break
+		}
+		if remaining >= LogLineBufLen {
+			end = start + LogLineBufLen
+		} else {
+			end = start + remaining
 		}
 	}
 
